5e4e75f5708e82b9d31b6bed52f76c6205ca3e15
vi4me <vitaliy.cherednichenko113@gmail.com>
2021-04-01 12:55:39 +0300

    update categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,14 +32,13 @@ class CategoriesController < ApplicationController
 
   def destroy
     @category.destroy
+    render json: {status: 'SUCCESS', message:'Deleted categories', data: @category},status: :ok
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
-      render json: {status: 'SUCCESS', message:'Deleted categories', data: @category},status: :ok
-
     end
 
     def category_params
